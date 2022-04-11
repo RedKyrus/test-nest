e5436928afc96b09961192f6cf8400f4206fa7b8
RedKyrus <ahehfldi2022@gmail.com>
2022-04-11 14:18:04 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //파이프
@@ -10,6 +12,9 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
+
+
+console.log(`http://localhost:${port}`);
---
+++ src/movies/movies.controller copy.ts
@@ -0,0 +1,41 @@
+// import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+// import { CreateMovieDto } from './dto/create-movie.dto';
+// import { UpdateMovieDto } from './dto/update-movie.dto';
+// import { Movie } from './entities/movie.entity';
+// import { MoviesService } from './movies.service';
+
+// @Controller('movies')
+// export class MoviesController {
+//   constructor(private readonly movieService: MoviesService) {}
+
+//   @Get()
+//   getAll() :Movie[] {
+//     return this.movieService.getAll();
+//   }
+
+//   @Get("search")
+//   search(@Query("year") searchingYear: string){
+//     return `We are searching for a movie with year: ${searchingYear}`
+//   }
+
+//   @Get(":id")
+//   getOne(@Param("id") movieId: number) {
+//     console.log(typeof movieId);
+//     return this.movieService.getOne(movieId);
+//   }
+
+//   @Post()
+//   create(@Body() movieData: CreateMovieDto) {
+//     return this.movieService.create(movieData);;
+//   }
+
+//   @Delete(":id")
+//   remove(@Param("id") movieId: number) {
+//     return this.movieService.deleteOne(movieId);
+//   }
+
+//   @Patch("/:id")
+//   patch(@Param("id") movieId: number, @Body() updateData: UpdateMovieDto) {
+//     return this.movieService.update(movieId, updateData);
+//   }
+// }
